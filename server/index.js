8aa42c46809b0b9730a0cd455bebc65ddf3799b5
gayetalib <gayetalib90@gmail.com>
2022-03-22 14:32:38 +0000

    add success

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express') 
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+const { json } = require('express')
+
+app.use(cors())
+app.use(express.json())
+
+// Connect to database
+const db = mysql.createConnection({
+    user:'root',
+    host: 'localhost',
+    password:'',
+    database: 'employeesystem'
+})
+
+//Insert data
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const age = req.body.age
+    const country = req.body.country
+    const position = req.body.position
+    const wage = req.body.wage
+
+    db.query("INSERT INTO employees (name, age, country, position, wage) VALUES (?, ?, ?, ?, ?)",
+    [name, age, country, position, wage], (err, result) => {
+        if (err) {
+            console.log(err);
+        }else{
+            res.send("Values inserted successfully");
+        }
+    })
+})
+
+app.listen(3001, () => {
+    console.log("Cool, your servier is running...!");
+})
