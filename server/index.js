3c1b8bfc8dac49de67b04c0eb49bc0fdb285f808
gayetalib <gayetalib90@gmail.com>
2022-03-22 16:32:15 +0000

    list employees

--- server/index.js
+++ server/index.js
@@ -33,6 +33,15 @@ app.post('/create', (req, res) => {
     })
 })
 
+app.get('/employees', (req, res)=>{
+   db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+        console.log(err);
+    }else{
+        res.send(result);
+    }})
+})
+
 app.listen(3001, () => {
     console.log("Cool, your servier is running...!");
 })
